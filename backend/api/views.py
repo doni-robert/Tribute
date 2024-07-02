d77b4bdcf9b4567e656650356ad4b78e078a712f
doni-robert <irobertndungu@gmail.com>
2024-07-02 17:28:30 +0300

    chore: create views for api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import County, Constituency, Ward, PollingStation, Entry
+from .serializers import CountySerializer, ConstituencySerializer, WardSerializer, PollingStationSerializer, EntrySerializer
 
-# Create your views here.
+class CountyListAPIView(generics.ListAPIView):
+    """API view to retrieve list of counties."""
+    queryset = County.objects.all()
+    serializer_class = CountySerializer
+
+class ConstituencyListAPIView(generics.ListAPIView):
+    """API view to retrieve list of constituencies."""
+    queryset = Constituency.objects.all()
+    serializer_class = ConstituencySerializer
+
+class WardListAPIView(generics.ListAPIView):
+    """API view to retrieve list of wards."""
+    queryset = Ward.objects.all()
+    serializer_class = WardSerializer
+
+class PollingStationListAPIView(generics.ListAPIView):
+    """API view to retrieve list of polling stations."""
+    queryset = PollingStation.objects.all()
+    serializer_class = PollingStationSerializer
+
+class EntryListCreateAPIView(generics.ListCreateAPIView):
+    """API view to retrieve list of entries or create new."""
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
